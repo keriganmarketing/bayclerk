e8d767047be5fc8f638ccc0f0a98107329bd1559
Bryan Baird <bryan@kerigan.com>
2019-05-15 14:02:26 -0400

    add file download template

---
+++ public/themes/wordplate/download-link.php
@@ -0,0 +1,3 @@
+<a class="download-link d-block" title="<?php $dlm_download->the_title(); ?>" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
+	<?php $dlm_download->the_title(); ?>
+</a>
