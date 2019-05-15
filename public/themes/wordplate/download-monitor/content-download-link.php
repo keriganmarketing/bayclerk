a0605d2e9bbb9d0a8ce4a4c128e8238f59efe384
Bryan Baird <bryan@kerigan.com>
2019-05-15 14:11:20 -0400

    wip

--- public/themes/wordplate/download-monitor/content-download-link.php
+++ public/themes/wordplate/download-monitor/content-download-link.php
@@ -1,3 +1,3 @@
-<a class="download-link d-block" title="<?php $dlm_download->the_title(); ?>" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
+<a class="download-link d-block mb-2" title="<?php $dlm_download->the_title(); ?>" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
 	<?php $dlm_download->the_title(); ?>
 </a>
