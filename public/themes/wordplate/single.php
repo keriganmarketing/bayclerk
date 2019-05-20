8690d8d75fe8032f19305327d21cfce873613224
Bryan Baird <bryan@kerigan.com>
2019-05-20 15:38:04 -0400

    remove sidebar from announcements

--- public/themes/wordplate/single.php
+++ public/themes/wordplate/single.php
@@ -1,7 +1,7 @@
 <?php
 $headerImageData = get_field('header_image');
 
-bladerunner('views.pages.index',[
+bladerunner('views.pages.single',[
     'headerImage' => $headerImageData['url'],
     'headline'    => get_field('headline'),
     'post'        => $post
